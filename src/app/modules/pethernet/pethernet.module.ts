893da9ebe4694f7df81b575b29e5d81ee92d0327
phatngluu <yadsendew@protonmail.com>
2021-08-12 09:25:26 +0700

    initial add vaccine dose form

--- src/app/modules/pethernet/pethernet.module.ts
+++ src/app/modules/pethernet/pethernet.module.ts
@@ -1,21 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PethernetRoutingModule } from './pethernet-routing.module';
+
+// Directives
+
+// Components
 import { PethernetComponent } from './pethernet.component';
 import { MedicalunitsComponent } from './pages/medicalunits/medicalunits.component';
 import { VaccinedosesComponent } from './pages/vaccinedoses/vaccinedoses.component';
+import { AddVaccineDoseComponent } from './shared/components/add-vaccine-dose/add-vaccine-dose.component';
 
+// Pipes
+
+// UI Modules
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @NgModule({
   declarations: [
+    // Directives
+
+
+    // Components
     PethernetComponent,
     MedicalunitsComponent,
-    VaccinedosesComponent
+    VaccinedosesComponent,
+    AddVaccineDoseComponent
+
+    // Pipes
+
+
   ],
   imports: [
+    // Generic Modules
     CommonModule,
-    PethernetRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    PethernetRoutingModule,
+
+
+    // UI Modules
+    NzFormModule,
+
+
   ]
 })
 export class PethernetModule { }
